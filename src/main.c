ce3162a5334e119388df66c1d9f1e93bdd5f4adb
aramyamal <aram@aramjamal.com>
2025-05-31 23:31:23 +0200

    feat: add initial program interface

--- src/main.c
+++ src/main.c
@@ -1,1 +1,28 @@
-int main(void) { return 0; }
+#include <stdio.h>
+
+int main(void) {
+    printf("Press h/j/k/l to choose slide direction, q to quit:\n");
+    int ch;
+    while ((ch = getchar()) != 'q') {
+        switch (ch) {
+        case 'h':
+            printf("slide left\n");
+            break;
+        case 'j':
+            printf("slide down\n");
+            break;
+        case 'k':
+            printf("slide up\n");
+            break;
+        case 'l':
+            printf("slide right\n");
+            break;
+        default:
+            printf("unknown key '%c'\n", ch);
+            break;
+        }
+        while (ch != '\n' && ch != EOF)
+            ch = getchar();
+    }
+    return 0;
+}
