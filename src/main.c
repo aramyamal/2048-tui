71f48c6e0efc4f97667236bfcc19082bec7d6d9d
aramyamal <aram@aramjamal.com>
2025-06-02 15:16:14 +0200

    feat: add ncurses game rendering

--- src/main.c
+++ src/main.c
@@ -1,48 +1,60 @@
-#include "uint16array.c"
-#include <stdio.h>
-
-typedef struct {
-    UInt16Array tiles;
-    UInt16Array previous;
-    size_t dimension;
-    size_t prev_left;
-    int32_t score;
-} GameState;
-
-GameState GameState_Init(size_t dimension) {
-    UInt16Array tiles = UInt16Array_Init(dimension, dimension);
-    return (GameState){
-        .tiles = tiles,
-        .previous = tiles,
-        .dimension = dimension,
-        .prev_left = 3,
-        .score = 0,
-    };
-}
+#include "game_state.c"
+#include <ncurses.h>
+#include <time.h>
 
 int main(void) {
-    printf("Press h/j/k/l to choose slide direction, q to quit:\n");
-    int ch;
-    while ((ch = getchar()) != 'q') {
+    // initialize ncurses
+    initscr();
+    cbreak();             // disable line buffering
+    noecho();             // don't echo pressed keys
+    keypad(stdscr, TRUE); // enable special keys
+
+    srand(time(NULL));
+    GameState *gs = GameState_create(4);
+
+    // clear screen and print initial state
+    clear();
+    GameState_print(gs);
+    printw("Press h/j/k/l to choose slide direction, q to quit:\n");
+    refresh();
+
+    int ch = 0;
+    bool game_over = false;
+
+    while ((ch = getch()) != 'q' && !game_over) {
         switch (ch) {
         case 'h':
-            printf("slide left\n");
+            printw("slide left\n");
             break;
         case 'j':
-            printf("slide down\n");
+            printw("slide down\n");
             break;
         case 'k':
-            printf("slide up\n");
+            printw("slide up\n");
             break;
         case 'l':
-            printf("slide right\n");
+            printw("slide right\n");
+            gs = GameState_slide_and_merge_right(gs);
             break;
         default:
-            printf("unknown key '%c'\n", ch);
+            printw("unknown key '%c'\n", ch);
             break;
         }
-        while (ch != '\n' && ch != EOF)
-            ch = getchar();
+        game_over = !GameState_add_random(gs);
+
+        // clear screen and redraw
+        clear();
+        GameState_print(gs);
+        printw("Press h/j/k/l to choose slide direction, q to quit:\n");
+        refresh();
     }
+
+    printw("Game Over!\nPress any key to quite...");
+    refresh();
+    getch(); // wait for final keypress
+
+    // cleanup ncurses
+    endwin();
+    GameState_destroy(gs);
     return 0;
 }
